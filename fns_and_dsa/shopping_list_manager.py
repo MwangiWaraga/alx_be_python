4aef49c274c65f9f5cb22e276c969a6a13e73c9b
Jackson Mwangi <jakanphonestore@gmail.com>
2024-07-14 15:02:24 +0300

    Fixed second buy

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,8 +1,8 @@
 def display_menu():
     print("\nShopping List Manager")
-    print("1. Add an item")
-    print("2. Remove an item")
-    print("3. View the list")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
     print("4. Exit")
 
 def add_item(shopping_list):
@@ -30,19 +30,19 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Choose an option (1-4): ")
-        
-        if choice == '1':
-            add_item(shopping_list)
-        elif choice == '2':
-            remove_item(shopping_list)
-        elif choice == '3':
-            view_list(shopping_list)
-        elif choice == '4':
-            print("Exiting the Shopping List Manager. Goodbye!")
-            break
-        else:
-            print("Invalid choice. Please choose a valid option.")
-
-
+        try:
+            choice = int(input("Choose an option (1-4): "))
+            if choice == 1:
+                add_item(shopping_list)
+            elif choice == 2:
+                remove_item(shopping_list)
+            elif choice == 3:
+                view_list(shopping_list)
+            elif choice == 4:
+                print("Exiting the Shopping List Manager. Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please choose a valid option.")
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 4.")
 
